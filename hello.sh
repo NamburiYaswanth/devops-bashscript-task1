04551f7a11019de204a33abaaa2e658054d2663a
NamburiYaswanth <112677183+NamburiYaswanth@users.noreply.github.com>
2025-11-10 12:05:40 +0530

    hello.sh

--- hello.sh
+++ hello.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #1. Write a script that asks for two numbers and operators(+,-,*) and performs the calculation
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
@@ -14,15 +13,10 @@ esac
 echo "Result: $result"
 
 #2. Write a script that counts how many files are in the current directory
-
-
 count=$(ls -1 | wc -l)
 echo "There are $count files in the current directory."
 
 #3.Write a script that asks for multiple names(in a loop) and greets each person, stopping when the user types "done"
-
-#!/bin/bash
-
 while true; do
   read -p "Enter a name (type 'done' to stop): " name
   if [ "$name" = "done" ]; then
@@ -33,7 +27,6 @@ while true; do
 done
 
 #4. Temperature converter: Convert celsius to fahrenheit (formula: F = C * 9/5 +32 )
-
 read -p "Enter temperature in Celsius: " c
 f=$(echo "scale=2; $c * 9 / 5 + 32")
 echo "$c°C = $f°F"
